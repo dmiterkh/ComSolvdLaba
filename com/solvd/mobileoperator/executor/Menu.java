fa3d220bcb879193979354039bc8834c96b30419
Dmitry Kharevich <dmiterkh@mail.ru>
2021-06-16 23:03:00 +0300

    HashSet and HashMap: functions researched

--- com/solvd/mobileoperator/executor/Menu.java
+++ com/solvd/mobileoperator/executor/Menu.java
@@ -228,7 +228,7 @@ public class Menu {
 						System.out.println("Sex of " + colleagueAr1.getListOfStaff().get(i).showStatus() + " " + indexSex + " is " + colleagueAr1.getListOfStaff().get(i).showSex());
 						System.out.println("Set sex to " + colleagueAr1.getListOfStaff().get(i).showStatus() + " " + indexSex + " (f/m/trans)");
 						sexIndex = in.nextLine();
-					} while((!("f".equals(index)))&&(!("m".equals(index)))&&(!("trans".equals(index))));
+					} while((!("f".equals(sexIndex)))&&(!("m".equals(sexIndex)))&&(!("trans".equals(sexIndex))));
 					colleagueAr1.getListOfStaff().get(i).setSex(sexIndex);
 				}
 			} else {
@@ -368,7 +368,7 @@ public class Menu {
 						System.out.println("Sex of " + colleagueLi1.getListOfStaff().get(i).showStatus() + " " + indexSex + " is " + colleagueLi1.getListOfStaff().get(i).showSex());
 						System.out.println("Set sex to " + colleagueLi1.getListOfStaff().get(i).showStatus() + " " + indexSex + " (f/m/trans)");
 						sexIndex = in.nextLine();
-					} while((!("f".equals(index)))&&(!("m".equals(index)))&&(!("trans".equals(index))));
+					} while((!("f".equals(sexIndex)))&&(!("m".equals(sexIndex)))&&(!("trans".equals(sexIndex))));
 					colleagueLi1.getListOfStaff().get(i).setSex(sexIndex);
 				}
 			} else {
@@ -390,7 +390,7 @@ public class Menu {
 						System.out.println("Sex of " + staff.showStatus() + " " + indexSex + " is " + staff.showSex());
 						System.out.println("Set sex to " + staff.showStatus() + " " + indexSex + " (f/m/trans)");
 						sexIndex = in.nextLine();
-					} while((!("f".equals(index)))&&(!("m".equals(index)))&&(!("trans".equals(index))));
+					} while((!("f".equals(sexIndex)))&&(!("m".equals(sexIndex)))&&(!("trans".equals(sexIndex))));
 					staff.setSex(sexIndex);
 				}
 			} else {
@@ -423,30 +423,30 @@ public class Menu {
 		System.out.println("====================================");
 		System.out.println("THAT'S IT for Scanner_2_LinkedList_Colleague");
 		System.out.println("====================================");
-		colleagueLi1.getListOfStaff().hashCode();
 
-//	}	
-//		
-//	
-//	
-//	public void showHashSetOperations() {	
-//		
-//		Scanner in = new Scanner(System.in);
-//		String index = "";
-//		
-//		Employee employee1 = new Employee(25, "F"); 
-//		employee1.setInfoAgain(26, "ff", 2000, "employee");
-//		Employee employee2 = new Employee(20, "M"); 
-//		Employee employee3 = new Employee(27, "F"); 
-//		BadEmployee badEmployee1 = new BadEmployee(35, "F"); 
-//		badEmployee1.setInfoAgain(36, "MM", 700, "bad employee");
-//		BadEmployee badEmployee2 = new BadEmployee(45, "M"); 	
-//		BadEmployee badEmployee3 = new BadEmployee(51, "M"); 	
-//		
-//		int employeeIndex = 0;
-//		int badEmployeeIndex = 0;
-//		int bossIndex = 0;
-//		int expiriencedApplicantIndex = 0;	
+
+	}	
+		
+	
+	
+	public void showHashSetOperations() {	
+		
+		Scanner in = new Scanner(System.in);
+		String index = "";
+		
+		Employee employee1 = new Employee(25, "F"); 
+		employee1.setInfoAgain(26, "ff", 2000, "employee");
+		Employee employee2 = new Employee(20, "M"); 
+		Employee employee3 = new Employee(27, "F"); 
+		BadEmployee badEmployee1 = new BadEmployee(35, "F"); 
+		badEmployee1.setInfoAgain(36, "MM", 700, "bad employee");
+		BadEmployee badEmployee2 = new BadEmployee(45, "M"); 	
+		BadEmployee badEmployee3 = new BadEmployee(51, "M"); 	
+		
+		int employeeIndex = 0;
+		int badEmployeeIndex = 0;
+		int bossIndex = 0;
+		int expiriencedApplicantIndex = 0;	
 		
 		System.out.println(" ");
 		System.out.println(" ");
@@ -497,6 +497,15 @@ public class Menu {
 		default:
 			break;				
 		}
+		
+		ColleagueHS colleagueHS2 = new ColleagueHS();
+		
+		colleagueHS2.setSetOfStaff(employee1);
+		colleagueHS2.setSetOfStaff(employee2);
+		colleagueHS2.setSetOfStaff(employee3);
+		colleagueHS2.setSetOfStaff(badEmployee1);
+		colleagueHS2.setSetOfStaff(badEmployee2);
+		
 	
 		do {
 			System.out.println("Would you like to get all of the information about all of the Employees? (y/n)");
@@ -504,14 +513,12 @@ public class Menu {
 		} while((!("y".equals(index)))&&(!("n".equals(index))));		
   
 		if ("y".equals(index)) {
-//			if ((colleagueAr1 != null)&&(!colleagueAr1.getListOfStaff().isEmpty())) {
+			if ((colleagueHS1 != null)&&(!colleagueHS1.getSetOfStaff().isEmpty())) {
 //			for(int i = 0; i < employeeIndex; i++) {
 //				System.out.println(colleagueAr1.getListOfStaff().get(i).toString());
 //				System.out.println("======");
 //			}
-
-			if ((colleagueHS1 != null)&&(!colleagueHS1.getSetOfStaff().isEmpty())) {
-				System.out.println(colleagueHS1.getSetOfStaff().toString());
+			System.out.println(colleagueHS1.getSetOfStaff().toString());
 			} else {
 				System.out.println("The required Storage is Null or Empty");	
 			}
@@ -519,8 +526,9 @@ public class Menu {
 			System.out.println("OK, move forward!");
 		}
     
-		sexIndex = "";
-		indexSex = 1;
+		
+		String sexIndex = "";
+		int indexSex = 1;
 	
 		do {
 			System.out.println("Would you like to set sex to all of the Employees? (y/n)");
@@ -534,7 +542,7 @@ public class Menu {
 //					System.out.println("Set sex to " + colleagueHS1.getSetOfStaff().get(i).showStatus() + " " + indexSex + " (f/m/trans)");
 //					sexIndex = in.nextLine();
 //					colleagueHS1.getSetOfStaff().get(i).setSex(sexIndex);
-//				}
+//				}				
 			} else {
 				System.out.println("The required Storage is Null or Empty");	
 			}			
@@ -562,33 +570,46 @@ public class Menu {
 			System.out.println("OK, move forward!");
 		}	
 		
+		Iterator iterator = colleagueHS1.getSetOfStaff().iterator();
+		while(iterator.hasNext()){
+			System.out.println(iterator.next().toString());
+			System.out.println("======");
+//?			System.out.println(iterator.next());
+//?			System.out.println("======");
+//?			System.out.println(iterator.next().remove());
+//?			System.out.println("======");
+		}		
+
+		System.out.println(colleagueHS1.getSetOfStaff().equals(colleagueHS2.getSetOfStaff()));
+		
 		System.out.println("====================================");
 		System.out.println("THAT'S IT for 3_HashSet_Colleague");
 		System.out.println("====================================");
-		colleagueHS1.getSetOfStaff().equals(colleagueHS1);
 		
-//	}	
-//		
-//	
-//	
-//	public void showHashMapOperations() {
-//		
-//		Scanner in = new Scanner(System.in);
-//		String index = "";
-//		
-//		Employee employee1 = new Employee(25, "F"); 
-//		employee1.setInfoAgain(26, "ff", 2000, "employee");
-//		Employee employee2 = new Employee(20, "M"); 
-//		Employee employee3 = new Employee(27, "F"); 
-//		BadEmployee badEmployee1 = new BadEmployee(35, "F"); 
-//		badEmployee1.setInfoAgain(36, "MM", 700, "bad employee");
-//		BadEmployee badEmployee2 = new BadEmployee(45, "M"); 	
-//		BadEmployee badEmployee3 = new BadEmployee(51, "M"); 	
-//		
-//		int employeeIndex = 0;
-//		int badEmployeeIndex = 0;
-//		int bossIndex = 0;
-//		int expiriencedApplicantIndex = 0;	
+		
+		
+	}	
+		
+	
+	
+	public void showHashMapOperations() {
+		
+		Scanner in = new Scanner(System.in);
+		String index = "";
+		
+		Employee employee1 = new Employee(25, "F"); 
+		employee1.setInfoAgain(26, "ff", 2000, "employee");
+		Employee employee2 = new Employee(20, "M"); 
+		Employee employee3 = new Employee(27, "F"); 
+		BadEmployee badEmployee1 = new BadEmployee(35, "F"); 
+		badEmployee1.setInfoAgain(36, "MM", 700, "bad employee");
+		BadEmployee badEmployee2 = new BadEmployee(45, "M"); 	
+		BadEmployee badEmployee3 = new BadEmployee(51, "M"); 	
+		
+		int employeeIndex = 0;
+		int badEmployeeIndex = 0;
+		int bossIndex = 0;
+		int expiriencedApplicantIndex = 0;	
 				
 		System.out.println(" ");
 		System.out.println(" ");
@@ -609,67 +630,126 @@ public class Menu {
 		
 		switch (employeeIndex){
 		case 1:
-			colleagueHM1.setMapOfStaff(employee1, employee1.showAge());
+			colleagueHM1.setMapOfStaff(11, employee1);
 			break;
 		case 2:
-			colleagueHM1.setMapOfStaff(employee1, employee1.showAge());
-			colleagueHM1.setMapOfStaff(employee2, employee2.showAge());
+			colleagueHM1.setMapOfStaff(11, employee1);
+			colleagueHM1.setMapOfStaff(12, employee2);
 			break;
 		case 3:
-			colleagueHM1.setMapOfStaff(employee1, employee1.showAge());
-			colleagueHM1.setMapOfStaff(employee2, employee2.showAge());
-			colleagueHM1.setMapOfStaff(employee3, employee3.showAge());
+			colleagueHM1.setMapOfStaff(11, employee1);
+			colleagueHM1.setMapOfStaff(12, employee2);
+			colleagueHM1.setMapOfStaff(13, employee3);
 			break;	
 		default:
 			break;				
 		}
 		switch (badEmployeeIndex){
 		case 1:
-			colleagueHM1.setMapOfStaff(badEmployee1, badEmployee1.showAge());
+			colleagueHM1.setMapOfStaff(21, badEmployee1);
 			break;
 		case 2:
-			colleagueHM1.setMapOfStaff(badEmployee1, badEmployee1.showAge());
-			colleagueHM1.setMapOfStaff(badEmployee2, badEmployee2.showAge());
+			colleagueHM1.setMapOfStaff(21, badEmployee1);
+			colleagueHM1.setMapOfStaff(22, badEmployee2);
 			break;
 		case 3:
-			colleagueHM1.setMapOfStaff(badEmployee1, badEmployee1.showAge());
-			colleagueHM1.setMapOfStaff(badEmployee2, badEmployee2.showAge());
-			colleagueHM1.setMapOfStaff(badEmployee3, badEmployee3.showAge());
+			colleagueHM1.setMapOfStaff(21, badEmployee1);
+			colleagueHM1.setMapOfStaff(22, badEmployee2);
+			colleagueHM1.setMapOfStaff(23, badEmployee3);
 			break;	
 		default:
 			break;				
 		}
+		
+		ColleagueHM colleagueHM2 = new ColleagueHM();
+		
+		colleagueHM2.setMapOfStaff(11, employee1);
+		colleagueHM2.setMapOfStaff(12, employee2);
+		colleagueHM2.setMapOfStaff(13, employee3);
+		colleagueHM2.setMapOfStaff(21, badEmployee1);
+		colleagueHM2.setMapOfStaff(22, badEmployee2);
+		
 		System.out.println(colleagueHM1.getMapOfStaff().toString());
-		System.out.println(colleagueHM1.getMapOfStaff().get(employee1));
+		System.out.println("======");
+		System.out.println("==get==");
+		System.out.println(colleagueHM1.getMapOfStaff().get(11));
+		System.out.println("====================================");
+
+		
+//	    ColleagueHM colleagueHM1 = new ColleagueHM();
+		Iterator mapIterator = colleagueHM1.getMapOfStaff().entrySet().iterator();
+		while (mapIterator.hasNext()) {
+			System.out.println(mapIterator.next());
+			System.out.println("======");
+//			Map.Entry<Integer, Staff> entry = mapIterator.next();
+//		    System.out.println("Key: " + entry.getKey());
+//		    System.out.println("Value: " + entry.getValue());
+		}	
+		
+//		Map<String, Integer> map = new HashMap<>();
+//		Iterator mapIterator = map.entrySet().iterator();
+//		while (mapIterator.hasNext()) {
+//		    Map.Entry<String, Integer> entry = mapIterator.next();
+//		    System.out.println("Key: " + entry.getKey());
+//		    System.out.println("Value: " + entry.getValue());
+//		}
 		
 		
-//	}
-//		
-//	
-//	
-//	public void showColletionOperations() {	
-//		
-//		Scanner in = new Scanner(System.in);
-//		String index = "";
-//		
-//		Employee employee1 = new Employee(25, "F"); 
-//		employee1.setInfoAgain(26, "ff", 2000, "employee");
-//		Employee employee2 = new Employee(20, "M"); 
-//		Employee employee3 = new Employee(27, "F"); 
-//		BadEmployee badEmployee1 = new BadEmployee(35, "F"); 
-//		badEmployee1.setInfoAgain(36, "MM", 700, "bad employee");
-//		BadEmployee badEmployee2 = new BadEmployee(45, "M"); 	
-//		BadEmployee badEmployee3 = new BadEmployee(51, "M"); 	
-//		
-//		int employeeIndex = 0;
-//		int badEmployeeIndex = 0;
-//		int bossIndex = 0;
-//		int expiriencedApplicantIndex = 0;	
-//		
-//		System.out.println(" ");
-//		System.out.println(" ");
-//		System.out.println("================================Scanner_3_HashSet_Colleague================================");	
-//		
+//		System.out.println(colleagueHM1.getMapOfStaff().containsValue(employee1));
+//		System.out.println(colleagueHM1.getMapOfStaff().containsKey(11));
+//		System.out.println(colleagueHM1.getMapOfStaff().equals(colleagueHM2.getMapOfStaff()));		
+//		System.out.println(colleagueHM1.getMapOfStaff().isEmpty());
+//		                   colleagueHM1.getMapOfStaff().remove(21, badEmployee1);
+//		                   colleagueHM1.getMapOfStaff().replace(22, badEmployee2, badEmployee3);
+//		System.out.println(colleagueHM1.getMapOfStaff().size());
+//		System.out.println(colleagueHM1.getMapOfStaff().toString());
+//		System.out.println(colleagueHM1.getMapOfStaff().get(11));
+//		System.out.println("======");
+//		System.out.println(colleagueHM1.getMapOfStaff().getClass());		
+//		System.out.println(colleagueHM1.getMapOfStaff().keySet());
+//		System.out.println(colleagueHM1.getMapOfStaff().values());
+//		System.out.println("======");
+//						   colleagueHM1.getMapOfStaff().replace(22, badEmployee1);
+//		System.out.println(colleagueHM1.getMapOfStaff().toString());
+//		System.out.println("======");
+
+		
+//		System.out.println(colleagueHM1.getMapOfStaff().entrySet());
+//		System.out.println(colleagueHM1.getMapOfStaff().compute(null, null));
+//		System.out.println(colleagueHM1.getMapOfStaff().computeIfAbsent(null, null));
+//		System.out.println(colleagueHM1.getMapOfStaff().computeIfPresent(null, null));
+//		System.out.println(colleagueHM1.getMapOfStaff().getOrDefault(21, badEmployee3));
+//		System.out.println(colleagueHM1.getMapOfStaff().merge(null, badEmployee3, null));
+//		System.out.println(colleagueHM1.getMapOfStaff().putIfAbsent(null, badEmployee3));
+//		System.out.println(colleagueHM1.getMapOfStaff().forEach(null));		
+		
+	}
+		
+	
+	
+	public void showColletionOperations() {	
+		
+		Scanner in = new Scanner(System.in);
+		String index = "";
+		
+		Employee employee1 = new Employee(25, "F"); 
+		employee1.setInfoAgain(26, "ff", 2000, "employee");
+		Employee employee2 = new Employee(20, "M"); 
+		Employee employee3 = new Employee(27, "F"); 
+		BadEmployee badEmployee1 = new BadEmployee(35, "F"); 
+		badEmployee1.setInfoAgain(36, "MM", 700, "bad employee");
+		BadEmployee badEmployee2 = new BadEmployee(45, "M"); 	
+		BadEmployee badEmployee3 = new BadEmployee(51, "M"); 	
+		
+		int employeeIndex = 0;
+		int badEmployeeIndex = 0;
+		int bossIndex = 0;
+		int expiriencedApplicantIndex = 0;	
+		
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println("================================Scanner_5_Collection_Colleague================================");	
+		
 	}
 		
 }
